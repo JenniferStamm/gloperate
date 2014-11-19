c357d22141f09cedbc8b9f1c8daff37716d9f72e
Dominik Mueller <isaak.dominik@gmail.com>
2014-11-19 15:08:22 +0100

    Add color property to CubeScapeExtended

--- source/examples/extended-painters/include/extended-painters/cubescape-extended/CubeScapeExtended.h
+++ source/examples/extended-painters/include/extended-painters/cubescape-extended/CubeScapeExtended.h
@@ -3,6 +3,7 @@
 #include <basic-painters/cubescape/CubeScape.h>
 
 #include <reflectionzeug/PropertyGroup.h>
+#include <reflectionzeug/Color.h>
 
 #include <extended-painters/extended_painters_api.h>
 
@@ -11,8 +12,11 @@ class EXTENDED_PAINTERS_API CubeScapeExtended : public CubeScape, public reflect
 public:
 	CubeScapeExtended(gloperate::ResourceManager & resourceManager);
 	virtual ~CubeScapeExtended();
-  std::string texture() const;
-  void setTexture(const std::string & texture);
+    std::string texture() const;
+    void setTexture(const std::string & texture);
+    reflectionzeug::Color color() const;
+    void setColor(const reflectionzeug::Color & color);
 protected:
-  std::string m_texture;
+    std::string m_texture;
+    reflectionzeug::Color m_color;
 };
--- source/examples/extended-painters/source/cubescape-extended/CubeScapeExtended.cpp
+++ source/examples/extended-painters/source/cubescape-extended/CubeScapeExtended.cpp
@@ -10,6 +10,10 @@ CubeScapeExtended::CubeScapeExtended(gloperate::ResourceManager & resourceManage
     auto texture = addProperty<std::string>(std::string("texture"), this, &CubeScapeExtended::texture,     &CubeScapeExtended::setTexture);
     std::vector<std::string> textures{ {"1.png", "2.png" , "3.png" , "4.png"} };
     texture->setOption("choices", textures);
+
+    auto color = addProperty<reflectionzeug::Color>(std::string("color"), this, &CubeScapeExtended::color,     &CubeScapeExtended::setColor);
+    // std::vector<reflectionzeug::Color> colors{ {reflectionzeug::Color(255, 0, 0), reflectionzeug::Color(0, 255, 0), reflectionzeug::Color(0, 0, 255)} };
+    // color->setOption("choices", colors);
 }
 
 CubeScapeExtended::~CubeScapeExtended()
@@ -24,4 +28,14 @@ std::string CubeScapeExtended::texture() const
 void CubeScapeExtended::setTexture(const std::string & texture)
 {
     m_texture = texture;
+}
+
+reflectionzeug::Color CubeScapeExtended::color() const
+{
+    return m_color;
+}
+
+void CubeScapeExtended::setColor(const reflectionzeug::Color & color)
+{
+    m_color = color;
 }
